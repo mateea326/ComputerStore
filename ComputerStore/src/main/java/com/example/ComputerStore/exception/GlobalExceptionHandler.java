1f681e57cfa74a303f7073ca871a2e3dd2697108
mateea326 <mateea.roman@s.unibuc.ro>
2026-05-04 23:25:03 +0300

    Fix incorrect import in GlobalExceptionHandler

--- ComputerStore/src/main/java/com/example/ComputerStore/exception/GlobalExceptionHandler.java
+++ ComputerStore/src/main/java/com/example/ComputerStore/exception/GlobalExceptionHandler.java
@@ -5,7 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
-import org.springframework.web.MethodArgumentNotValidException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
