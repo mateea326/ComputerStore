47f86dcafa1419e569ec7cc48b00abb0dc311eaa
ZugravuAlexandra <alexandrazugravu3@gmail.com>
2026-05-05 15:45:55 +0300

    fix merge issues

--- ComputerStore/src/main/java/com/example/ComputerStore/exception/ResourceNotFoundException.java
+++ ComputerStore/src/main/java/com/example/ComputerStore/exception/ResourceNotFoundException.java
@@ -8,4 +8,8 @@ public class ResourceNotFoundException extends RuntimeException {
     public ResourceNotFoundException(String resourceName, Integer id) {
         super(resourceName + " with id " + id + " was not found");
     }
+
+    public ResourceNotFoundException(String resourceName, String fieldName, Object fieldValue) {
+        super(String.format("%s not found with %s : '%s'", resourceName, fieldName, fieldValue));
+    }
 }
