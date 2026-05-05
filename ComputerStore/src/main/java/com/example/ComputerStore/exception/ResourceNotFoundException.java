748df7d175b26be32289507831c7faa7029f1844
ZugravuAlexandra <alexandrazugravu3@gmail.com>
2026-05-05 16:06:45 +0300

    final conflict and build fix

--- ComputerStore/src/main/java/com/example/ComputerStore/exception/ResourceNotFoundException.java
+++ ComputerStore/src/main/java/com/example/ComputerStore/exception/ResourceNotFoundException.java
@@ -11,7 +11,7 @@ public class ResourceNotFoundException extends RuntimeException {
     private final Object fieldValue;
 
     public ResourceNotFoundException(String resourceName, String fieldName, Object fieldValue) {
-        super(String.format("%s not found with %s: '%s'", resourceName, fieldName, fieldValue));
+        super(String.format("%s not found with %s : '%s'", resourceName, fieldName, fieldValue));
         this.resourceName = resourceName;
         this.fieldName = fieldName;
         this.fieldValue = fieldValue;
@@ -23,14 +23,15 @@ public class ResourceNotFoundException extends RuntimeException {
         this.fieldName = "id";
         this.fieldValue = null;
     }
-
-<<<<<<< HEAD
-    public ResourceNotFoundException(String resourceName, String fieldName, Object fieldValue) {
-        super(String.format("%s not found with %s : '%s'", resourceName, fieldName, fieldValue));
+    
+    public ResourceNotFoundException(String resourceName, Integer id) {
+        super(resourceName + " with id " + id + " was not found");
+        this.resourceName = resourceName;
+        this.fieldName = "id";
+        this.fieldValue = id;
     }
-=======
+
     public String getResourceName() { return resourceName; }
     public String getFieldName() { return fieldName; }
     public Object getFieldValue() { return fieldValue; }
->>>>>>> origin/main
 }
